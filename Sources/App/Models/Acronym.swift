fb1b5dc7f14e0df9b8a8378190fd51d522e1e1a7
hoyelam <hoyelam96@gmail.com>
2019-10-23 21:29:43 +0200

    Chapter 9

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -5,17 +5,32 @@ final class Acronym: Codable {
     var id      : Int?
     var short   : String
     var long    : String
+    var userId  : User.ID
     
-    init(short: String, long: String) {
+    init(short: String, long: String, userId: User.ID) {
         self.short  = short
         self.long   = long
+        self.userId = userId
     }
 }
 
 extension Acronym: PostgreSQLModel  { }
 
-extension Acronym: Migration        { }
+extension Acronym: Migration        {
+    static func prepare(on connection: PostgreSQLConnection) -> EventLoopFuture<Void> {
+        return Database.create(self, on: connection) { builder in
+            try addProperties(to: builder)
+            builder.reference(from: \.userId, to: \User.id)
+        }
+    }
+}
 
 extension Acronym: Content          { }
 
 extension Acronym: Parameter        { }
+
+extension Acronym {
+    var user: Parent<Acronym, User> {
+        return parent(\.userId)
+    }
+}
