170fe5273be482399ab2e95b1c31216b546bd16a
hoyelam <hoyelam96@gmail.com>
2019-10-29 20:15:57 +0100

    Chapter 10: Add sibling relationships

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -33,4 +33,8 @@ extension Acronym {
     var user: Parent<Acronym, User> {
         return parent(\.userId)
     }
+    
+    var categories: Siblings<Acronym, Category, AcronymCategoryPivot> {
+        return siblings()
+    }
 }
